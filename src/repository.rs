569f7f6a13ff84a9b0b6975f80b289722038f403
Gustavo Noronha Silva <gustavo@noronha.dev.br>
2024-09-08 12:26:04 -0300

    Fix init behaviour when .git already exists

--- src/repository.rs
+++ src/repository.rs
@@ -1,11 +1,36 @@
 use anyhow::Result;
+use std::env;
 use std::fs;
+use std::io;
+
+fn ensure_dir(path: &str) -> io::Result<()> {
+    fs::create_dir(path).or_else(|e| {
+        if matches!(e.kind(), io::ErrorKind::AlreadyExists) {
+            Ok(())
+        } else {
+            Err(e)
+        }
+    })
+}
 
 pub fn init() -> Result<()> {
-    fs::create_dir(".git")?;
-    fs::create_dir(".git/objects")?;
-    fs::create_dir(".git/refs")?;
-    fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
-    println!("Initialized git directory");
+    let is_reinit = fs::exists(".git")?;
+
+    ensure_dir(".git")?;
+    ensure_dir(".git/objects")?;
+    ensure_dir(".git/refs")?;
+
+    if is_reinit {
+        println!(
+            "Reinitialized existing Git repository in {}",
+            env::current_dir()?.display()
+        )
+    } else {
+        fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
+        println!(
+            "Initialized empty Git repository in {}",
+            env::current_dir()?.display()
+        );
+    }
     Ok(())
 }
